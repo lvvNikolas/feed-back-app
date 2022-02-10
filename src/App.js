b6b95cb3bc3a190fa862f233c8ea6ec86c3cb78a
Nik <lvvniko7@gmail.com>
2022-02-10 09:40:01 -0800

    section 2 lesson 005

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 function App(){
+    const title = 'Blog Post'
+    const body = 'This is my blog post'
+    const comments = [
+        {id: 1, text: 'Comment one'},
+        {id: 2, text: 'Comment two'},
+        {id: 3, text: 'Comment three'},
+    ]
     return (
         <div className="container">
-        <h1>My App</h1>
-        <p>Hello</p>
-        
+        <h1>{title.toLocaleUpperCase()}</h1>
+        <p>{body}</p>
+        {5 + 5}
+        <div className="comments">
+            <h3>Comments ({comments.length})</h3>
+            <ul>
+                {comments.map((comment, index) => (
+                    <li key={index}>{comment.text}</li>
+                ))}
+            </ul>
+        </div>
         </div>
         
     )
