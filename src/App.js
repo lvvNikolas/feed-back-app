ee32b296b2bf173a684eeb3d98ee232e8b5b9aae
Nik <lvvniko7@gmail.com>
2022-02-10 10:56:40 -0800

    section 3 lesson 001

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
+import Header from './components/Header'
 function App(){
-    const title = 'Blog Post'
-    const body = 'This is my blog post'
-    const comments = [
-        {id: 1, text: 'Comment one'},
-        {id: 2, text: 'Comment two'},
-        {id: 3, text: 'Comment three'},
-    ]
     return (
+        <>
+        <Header text={true}/>  
         <div className="container">
-        <h1>{title.toLocaleUpperCase()}</h1>
-        <p>{body}</p>
-        {5 + 5}
-        <div className="comments">
-            <h3>Comments ({comments.length})</h3>
-            <ul>
-                {comments.map((comment, index) => (
-                    <li key={index}>{comment.text}</li>
-                ))}
-            </ul>
-        </div>
-        </div>
-        
+        <h1>My App</h1>
+    </div>
+    </>
+      
     )
+    
+        
+    
 }
 
 export default App
