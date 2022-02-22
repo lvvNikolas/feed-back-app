10c2d69e7b8e3594a8c996ee92cbf68e569a7ace
Nik <lvvniko7@gmail.com>
2022-02-22 09:25:29 -0800

    section 5 lesson 003

--- src/App.js
+++ src/App.js
@@ -6,40 +6,40 @@ import FeedbackList from './components/FeedbackList'
 import FeedbackStats  from './components/FeedbackStats'
 import FeedbackForm from './components/FeedbackForm'
 import FeedbackData from './data/FeedbackData'
+import AboutIconLink from './components/AboutIconLink'
 import AboutPage from './pages/AboutPage'
-
 function App(){
-    const [feedback, setFeedback] = useState(FeedbackData)
-    const addFeedback = (newFeedback) => {
-        newFeedback.id = uuidv4()
-         setFeedback([newFeedback,...feedback])
-    }
+  const [feedback, setFeedback] = useState(FeedbackData)
+  const addFeedback = (newFeedback) => {
+      newFeedback.id = uuidv4()
+       setFeedback([newFeedback,...feedback])
+  }
 
-    const deleteFeedback = (id) => {
-        if(window.confirm('Are you sure want to delete ?')){
-            setFeedback(feedback.filter((item) => item.id !== id))
-        }
-       
-    }
-    return (
-        <Router>
-        <Header />  
-        <div className="container">
-        <Route exact path="/">
-        <FeedbackForm handleAdd={addFeedback} />
-        <FeedbackStats feedback={feedback}/>
-        <FeedbackList feedback={feedback}
-        handleDelete={deleteFeedback} />
-        </Route>
-        <Route path='/about' components={AboutPage}/>
-         
-    </div>
-    </Router>
+  const deleteFeedback = (id) => {
+      if(window.confirm('Are you sure want to delete ?')){
+          setFeedback(feedback.filter((item) => item.id !== id))
+      }
+     
+  }
+  return (
+      <Router>
+      <Header />  
+      <div className="container">
+      <Route exact path="/">
+      <FeedbackForm handleAdd={addFeedback} />
+      <FeedbackStats feedback={feedback}/>
+      <FeedbackList feedback={feedback}
+      handleDelete={deleteFeedback} />
+      </Route>
+      <Route path='/about' components={AboutPage}/>
+     <AboutIconLink/>  
+  </div>
+  </Router>
+    
+  )
+  
       
-    )
-    
-        
-    
+  
 }
 
 export default App
