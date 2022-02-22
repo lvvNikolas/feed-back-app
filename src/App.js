facb4c0bab75e29f1ab66961f06cb40ba90e500d
Nik <lvvniko7@gmail.com>
2022-02-22 09:35:29 -0800

    section 5 lesson 005

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import {v4 as uuidv4} from 'uuid'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import {useState} from 'react'
 import Header from './components/Header'
 import FeedbackList from './components/FeedbackList'
@@ -25,13 +25,18 @@ function App(){
       <Router>
       <Header />  
       <div className="container">
-      <Route exact path="/">
+      <Routes>
+      <Route exact path="/" element={
+          <>
       <FeedbackForm handleAdd={addFeedback} />
       <FeedbackStats feedback={feedback}/>
       <FeedbackList feedback={feedback}
-      handleDelete={deleteFeedback} />
+      handleDelete={deleteFeedback} />  
+          </>
+      }>
       </Route>
-      <Route path='/about' components={AboutPage}/>
+      <Route path='/about' element={<AboutPage/>}/>
+      </Routes>
      <AboutIconLink/>  
   </div>
   </Router>
