d82ee134ecfe74e9f908c54d28fc62f2fb14501b
Nik <lvvniko7@gmail.com>
2022-02-23 09:44:03 -0800

    section 6 lesson 003

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import {v4 as uuidv4} from 'uuid'
+
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import {useState} from 'react'
 import Header from './components/Header'
 import FeedbackList from './components/FeedbackList'
 import FeedbackStats  from './components/FeedbackStats'
@@ -8,20 +7,8 @@ import FeedbackForm from './components/FeedbackForm'
 import AboutIconLink from './components/AboutIconLink'
 import AboutPage from './pages/AboutPage'
 import { FeedbackProvider } from './context/FeedbackContext'
-import FeedbackData from './data/FeedbackData'
 function App(){
-  const [feedback, setFeedback] = useState(FeedbackData)
-  const addFeedback = (newFeedback) => {
-      newFeedback.id = uuidv4()
-       setFeedback([newFeedback,...feedback])
-  }
 
-  const deleteFeedback = (id) => {
-      if(window.confirm('Are you sure want to delete ?')){
-          setFeedback(feedback.filter((item) => item.id !== id))
-      }
-     
-  }
   return (
       <FeedbackProvider>
       <Router>
@@ -30,10 +17,9 @@ function App(){
       <Routes>
       <Route exact path="/" element={
           <>
-      <FeedbackForm handleAdd={addFeedback} />
+      <FeedbackForm/>
       <FeedbackStats/>
-      <FeedbackList
-      handleDelete={deleteFeedback} />  
+      <FeedbackList />  
           </>
       }>
       </Route>
